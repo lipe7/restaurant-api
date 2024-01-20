76e261a3ff5ed493a96d1b6b43243a7d16928d25
felipe <felipec165@gmail.com>
2024-01-19 23:45:26 -0300

    endpoints orders

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('orders')->group(function () {
+    Route::post('/', [OrderController::class, 'createOrder']);
+    Route::get('{id}', [OrderController::class, 'consultOrder']);
+    Route::get('processing', [OrderController::class, 'listOrdersInProcessing']);
+    Route::delete('{id}/cancel', [OrderController::class, 'cancelOrder']);
 });
