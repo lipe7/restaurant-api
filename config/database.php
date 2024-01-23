d9e852ed6bae4338857a0225f931916ab09af755
felipe <felipec165@gmail.com>
2024-01-22 21:26:18 -0300

    laravel telescope

--- config/database.php
+++ config/database.php
@@ -121,7 +121,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
